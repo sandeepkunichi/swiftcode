0b725f9af9c0c38b56c21419db143cfd0e95717f
sandeepkunichi <sandeepkunichi@gmail.com>
2016-09-04 15:48:51 +0530

    results added to admin view

--- app/controllers/admin/HomeController.java
+++ app/controllers/admin/HomeController.java
@@ -2,6 +2,7 @@ package controllers.admin;
 
 import models.AppUser;
 import models.test.Test;
+import models.test.TestSession;
 import play.data.Form;
 import play.data.FormFactory;
 import play.mvc.Controller;
@@ -11,6 +12,7 @@ import services.SessionService;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Sandeep.K on 8/14/2016.
@@ -26,7 +28,12 @@ public class HomeController extends Controller {
     public Result index() throws IOException {
         List<Test> tests = Test.find.all();
         List<AppUser> users = AppUser.find.all();
-        return ok(views.html.admin.index.render(tests, sessionService.getSessionUser(), users));
+        List<TestSession> testSessions = TestSession.find.all().stream().map(testSession -> {
+            testSession.test = Test.find.byId(testSession.test.id);
+            testSession.testTaker = AppUser.find.byId(testSession.testTaker.id);
+            return testSession;
+        }).collect(Collectors.toList());
+        return ok(views.html.admin.index.render(tests, sessionService.getSessionUser(), users, testSessions));
     }
 
     public Result createTest(){
