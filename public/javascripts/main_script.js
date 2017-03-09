a5d3b8f88bb431fba785ed638e8e2b04e8ca4193
sandeepkunichi <sandeep.k@betsol.com>
2017-03-09 19:42:25 +0530

    Website changes

--- public/javascripts/main_script.js
+++ public/javascripts/main_script.js
@@ -332,6 +332,19 @@ function getQuad(position) {
     }
 
 }
+function goToByScroll(id){
+      // Remove "link" from the ID
+    id = id.replace("link-", "");
+      // Scroll
+    $('html,body').animate({scrollTop: $("#"+id).offset().top}, 'slow');
+}
+
+$("#mainNavbarContent > ul > li > a").click(function(e) {
+      // Prevent a page reload when a link is pressed
+    e.preventDefault();
+      // Call the scroll function
+    goToByScroll(this.id);
+});
 
 //Event Handlers
 window.addEventListener("resize", onWindowResize);
